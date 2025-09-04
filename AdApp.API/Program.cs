a0bebcfab40cd5c2e044db0a17b471641fa8c5b1
keetaeza <kdobrijvecor@gmail.com>
2025-09-04 17:00:34 +0400

    добавила файл для теста, внесла изменения в program.cs

--- AdApp.API/Program.cs
+++ AdApp.API/Program.cs
@@ -31,10 +31,13 @@ namespace AdApp.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
             }
 
             app.UseHttpsRedirection();
